a971af04ba566a11ae86971f9d49039e32a016e2
Rhyan Clark Randriamisaina <rhrandri@c1r9s12.42antananarivo.mg>
2026-02-23 10:43:19 +0300

    get_next_line_utils.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,21 @@
 /*   By: rhrandri <rhrandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 09:22:18 by rhrandri          #+#    #+#             */
-/*   Updated: 2026/02/23 09:22:24 by rhrandri         ###   ########.fr       */
+/*   Updated: 2026/02/23 10:27:00 by rhrandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*buf;
+	char		*nl;
+	char		*r;
+	char		tmp;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	
+}
