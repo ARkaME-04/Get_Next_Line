e9a2a577f59ef6bf83f49e3a30f33a31a28eacd4
Rhyan Clark Randriamisaina <rhrandri@c1r8s14.42antananarivo.mg>
2026-02-25 08:07:33 +0300

    utils done... again

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rhrandri <rhrandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 09:26:07 by rhrandri          #+#    #+#             */
-/*   Updated: 2026/02/25 07:35:15 by rhrandri         ###   ########.fr       */
+/*   Updated: 2026/02/25 08:05:59 by rhrandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ size_t	ft_strlen(char *str)
 	size_t	i;
 
 	if (!str)
-		return (NULL);
+		return (0);
+	i = 0;
 	while (str[i])
 		i++;
 	return (i);
@@ -33,6 +34,7 @@ char	*ft_strdup(const char *s1)
 	s2 = (char *) malloc(ft_strlen(s1) + 1);
 	if (!s2)
 		return (NULL);
+	i = 0;
 	while (s1[i])
 	{
 		s2[i] = s1[i];
@@ -42,11 +44,13 @@ char	*ft_strdup(const char *s1)
 	return (s2);
 }
 
-char	ft_strchr(char *str, int c)
+char	*ft_strchr(char *str, int c)
 {
 	unsigned char	ch;
 
-	ch = c;
+	ch = (unsigned char)c;
+	if (!str)
+		return (NULL);
 	while (*str)
 	{
 		if (*str == ch)
@@ -66,7 +70,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	res = (char *) malloc(ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char);
+	res = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
 	i = 0;
@@ -88,14 +92,13 @@ char	*ft_substr(char *str, unsigned int start, size_t len)
 	if (!str)
 		return (NULL);
 	if (start >= ft_strlen(str))
-		return (malloc(1));
-	if (start < 0)
-		return (NULL);
-	if (len > ft_strlen(s + start))
-		len = ft_strlen(s + start);
-	s = malloc(len + 1) * sizeof(char);
+		return (ft_strdup(""));
+	if (len > ft_strlen(str + start))
+		len = ft_strlen(str + start);
+	s = malloc((len + 1) * sizeof(char));
 	if (!s)
 		return (NULL);
+	i = 0;
 	while (i < len)
 	{
 		s[i] = str[i + start];
